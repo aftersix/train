0019688e144e13593c680d32a21702c0d65329bd
aftersix <alex.arnow@gmail.com>
2017-03-06 13:15:35 -0500

    added some UI elements

--- routes/predictions.js
+++ routes/predictions.js
@@ -1,16 +1,6 @@
 var Sugar = require('sugar');
 var request = require('request');
 
-
-//var mbtaFeed; //needs to be a universal variable to pass outside of the 
-
-
-
-
-
-console.log("ran predictions.js");
-
-
 exports.predictions = function(req,res) {
 
 var salemSchedule = {table: [] };
@@ -32,9 +22,11 @@ var trainAlerts;
 						for (var k=0; k<route['direction'].length; k++){
 							var direction = route['direction'][k];
 							for (var l=0; l<direction['trip'].length; l++){
+								var difference = direction['trip'][l]['sch_arr_dt'] - direction['trip'][l]['pre_dt'];
+								difference = Math.abs(difference);
 								direction['trip'][l]['sch_arr_dt'] = Sugar.Date.format(Sugar.Date.create(direction['trip'][l]['sch_arr_dt']*1000)	,  '{hh}:{mm}');							
 								direction['trip'][l]['pre_dt'] = Sugar.Date.format(Sugar.Date.create(direction['trip'][l]['pre_dt']*1000)	,  '{hh}:{mm}');
-								salemSchedule.table.push({train_direction: route['direction'][k]['direction_name'], train_name: direction['trip'][l]['trip_name'], scheduleTime: direction['trip'][l]['sch_arr_dt'],predictedTime:direction['trip'][l]['pre_dt']});
+								salemSchedule.table.push({train_direction: route['direction'][k]['direction_name'], train_name: direction['trip'][l]['trip_name'], scheduleTime: direction['trip'][l]['sch_arr_dt'],predictedTime:direction['trip'][l]['pre_dt'],difference:difference});
 							}
 						}
 					}
@@ -54,7 +46,7 @@ var trainAlerts;
 		  if (!error && response.statusCode == 200) {
 				for (var i=0; i<trainAlert['alerts'].length; i++){  
 					
-					console.log(trainAlert['alerts'][i]['short_header_text']);
+					//console.log(trainAlert['alerts'][i]['short_header_text']);
 				 }  
 		  }
 		trainAlerts = trainAlert;  
@@ -65,8 +57,7 @@ var trainAlerts;
 
 	Promise.all([salemPromise, alertPromise]).then(values => { 
 	  res.render('predictions', {title:'predictions' , salemSchedule:salemSchedule , trainAlerts:trainAlerts});
-	  console.log("ran predictions.js exports module - promise done.");
-	});
+	  	});
 
 	
 	
